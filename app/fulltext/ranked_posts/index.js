12777fdc46a97639457b8353a15f923dc062b102
Lincoln Stoll <l@lds.li>
2010-11-30 09:22:14 +1100

    Update FTI function to suit new schema, add weighting tweaks

--- app/fulltext/ranked_posts/index.js
+++ app/fulltext/ranked_posts/index.js
@@ -1,11 +1,24 @@
 function(doc) {
+  var ret = new Document();
   if(doc.kind == 'post') {
-    var ret = new Document();
-    ret.add(doc.Body, { boost: 2.0 });
-    for(i in doc.answers) {
-      ret.add(doc.answers[i].Body, { boost: 1.0 });
+    // Child Post (answer)
+    var scoreBoost = doc.Score / 20; // Arbitrary number. Think more later.
+    if (doc.PostTypeId === 2 || doc.PostTypeId === '2') {
+      ret.add(doc.Body, { boost: 1.25 + scoreBoost });
     }
-    return ret;
+    // Question
+    else {
+      ret.add(doc.Body, { boost: 2.0 + scoreBoost });
+    }
+    // Tags
+    if (doc.Tags) {
+      var tags = doc.Tags.replace('<', '').replace('>',' ');
+      ret.add(tags, { boost: 3 });
+    }
   }
+  else if (doc.kind == 'comment') {
+    ret.add(doc.Text, { boost: 1 });
+  }
+  return ret;
 }
 
